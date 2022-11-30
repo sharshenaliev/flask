ed4157f1618943d8030d962c0b59d544fdf95ad9
sharshenaliev <59019588+sharshenaliev@users.noreply.github.com>
2022-11-30 14:45:38 +0600

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def pageNotFound(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=7000))
